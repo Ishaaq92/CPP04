a26f6568b23b546527506baf35212bfa66b32f2f
Ishaaq Ahmed <a.ishaaq@icloud.com>
2025-08-22 12:44:48 +0100

    Looking goody. I removed the virtual keyword to show that it will call parent's function instead of the implementation of it's own class

--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -13,6 +13,6 @@ class WrongAnimal
 		WrongAnimal(std::string type);
 		WrongAnimal(void);
 		~WrongAnimal(void);
-		virtual void	makeSound(void) const;
+		void	makeSound(void) const;
 		std::string		getType(void) const;
 };
