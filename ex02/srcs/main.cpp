f91644b5d4de29ba398c18c3275b4859d77fb2f8
ishaaq <a.ishaaq@icloud.com>
2026-03-01 10:06:42 +0000

    removed the old one

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,11 +6,10 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 12:16:36 by ishaaq            #+#    #+#             */
-/*   Updated: 2026/03/01 10:03:29 by isahmed          ###   ########.fr       */
+/*   Updated: 2026/03/01 10:06:19 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
 #include "Brain.hpp"
